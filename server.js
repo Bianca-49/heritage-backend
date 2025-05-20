be7bce6b5a9dc61404db513150e09b78f2723760
Bianca Negoescu <negoescu.biancaelena@gmail.com>
2025-05-21 01:20:50 +0300

    Actualizat admin.html si altele

--- server.js
+++ server.js
@@ -75,7 +75,7 @@ app.get('/auth/google/callback',
   passport.authenticate('google', { session: false }),
   (req, res) => {
     const { token } = req.user;
-    res.redirect(`http://localhost:5500/map.html?token=${token}`);
+    res.redirect(`https://gilded-gnome-4e9c2e.netlify.app/index.html?token=${token}`);
   }
 );
 
